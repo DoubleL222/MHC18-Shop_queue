6378fa8b78ab9e4c36c37e1c0be2307075807d12
Jordi.GonzalezCa <jordi.gonzalezca@e-campus.uab.cat>
2018-10-28 10:57:31 +0100

    PWP edition

--- Code/People.py
+++ Code/People.py
@@ -1,2 +1,15 @@
 
-class person():
+class Person():
+	def __init__(self, _id, pos, avSpeed):
+		self.id = _id
+		self.pos = pos
+		self.avSpeed = avSpeed
+		self.actSpeed = avSpeed
+
+class Customer(Person):
+	def __init__(self, time):
+		self.timeStore = time
+
+class Cashier(Person):
+	def __init__(self, inCash):
+		self.inCash = inCash
